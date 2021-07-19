63c5a8f427a45a07d998b9db4474e9c5255e2fa4
firasel <mdrasel601390@gmail.com>
2021-07-20 03:22:54 +0600

    footer is complete

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
 import NavBar from '../NavBar/NavBar';
 
 const Home = () => {
     return (
         <div>
             <NavBar/>
+            <Footer/>
         </div>
     );
 };
