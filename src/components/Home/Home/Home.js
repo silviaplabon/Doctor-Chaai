d1f2ebe2e2a2606f7cdc4d4418d4d9c7ad7c45e4
firasel <mdrasel601390@gmail.com>
2021-07-20 02:39:27 +0600

    navbar is complete

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    );
+};
+
+export default Home;
