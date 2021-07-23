e823732cafe0d95bcf76c395c66bf1431fef6b8f
firasel <mdrasel601390@gmail.com>
2021-07-23 17:35:25 +0600

    Complete the find doctor and take appointment section

---
+++ src/components/Home/TakeAppointment/TakeAppointment.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './TakeAppointment.scss';
+
+const TakeAppointment = () => {
+    return (
+        <div className="container-sm py-4">
+            <div className="row row-cols-1 row-cols-sm-2 g-4">
+            <div className="cols order-1 d-flex align-items-center">
+                    <div className="p-4">
+                        <h2 className="titleDesign1 my-3">Urgent Online Care</h2>
+                        <p className="text-muted mt-3">Tell us your health concern and we will assign you a top doctor in 2 minute.</p>
+                        <button className="btn rounded-pill btnDesign1 mt-3">Take Appointment</button>
+                    </div>
+                </div>
+                <div className="cols order-0 order-sm-2 p-4" >
+                    <div className="appointmentImg">
+                        <img src="https://i.ibb.co/nLN9MfX/doctor-work1.jpg" alt="find doctor"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TakeAppointment;
