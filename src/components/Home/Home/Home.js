30d2d781690a9234098d84793665e4a2a2094081
tahmidmahdi <tahmid5550@gmail.com>
2021-07-24 22:25:17 +0600

    take appointment

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Features from '../Features/Features';
 import FindDoctor from '../FindDoctor/FindDoctor';
 import Footer from '../Footer/Footer';
 import NavBar from '../NavBar/NavBar';
+import OurService from '../OurService/OurService';
 import TakeAppointment from '../TakeAppointment/TakeAppointment';
 import TalentedStaff from '../TalentedStaff/TalentedStaff';
 
@@ -10,9 +12,15 @@ const Home = () => {
         <div>
             <NavBar/>
             <FindDoctor/>
+            <Features />
             <TakeAppointment/>
+<<<<<<< HEAD
             <TalentedStaff />
+=======
+            <OurService/>
+>>>>>>> 7ce9e7c26e2635259cdb494796209a1a5d29dbc0
             <Footer/>
+
         </div>
     );
 };
