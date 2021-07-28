5464bc39647d238f13b705bb62798835cd0c3cc1
tahmidmahdi <tahmid5550@gmail.com>
2021-07-28 18:57:46 +0600

    adding animation to take appointment

--- src/components/Home/TakeAppointment/TakeAppointment.js
+++ src/components/Home/TakeAppointment/TakeAppointment.js
@@ -16,7 +16,7 @@ const TakeAppointment = () => {
                         <h2 className="py-5">Quick Appointment</h2>
                         <h1>HAVEN'T BOOKED YET? </h1>
                         <p>Hospital Management System or HMS Software is mainly a system that can help to maintain any hospital activities properly and easily. Moreover, </p>
-                        <button onClick={handleClick} className="btn rounded-pill btnDesign1 mt-3 p-4">Appoint Your Doctor</button>
+                        <button onClick={handleClick} className="btn rounded-pill btnDesign1 mt-3 px-5 py-4">Appoint Your Doctor</button>
                     </div>
                     <div className="col appointmentImg">
                         <img src='https://i.ibb.co/nny0MK7/national-cancer-institute-NFvd-KIhx-Yl-U-unsplash.jpg' alt="take appointment" />
