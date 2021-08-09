19804b4e074d8cd004f8d68323ccc3604cd00bc9
silviaplabon <silviaplabon@gmail.com>
2021-08-10 00:20:31 +0600

    sole conflict

--- src/components/Home/Doctors/DoctorsCollection/DoctorsCollection.js
+++ src/components/Home/Doctors/DoctorsCollection/DoctorsCollection.js
@@ -19,7 +19,7 @@ const Doctors = () => {
 
     return (
         <div className="container mb-5" style={{ marginTop: '120px' }}>
-            <h6 className="text-center mb-1 mt-5" style={{ color: '#1dc0bf' }}>MEET OUR DOCTORS</h6>
+            <h6 className="text-center mb-1 mt-5" style={{ color: '#1dc0bf' }}>MEET OURS DOCTORS</h6>
             <h2 className="text-center mb-5" style={{ color: '#050546' }}>Top Rated Specialists</h2>
             <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
                 {
