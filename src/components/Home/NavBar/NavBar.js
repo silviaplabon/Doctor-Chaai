6fe8e3e3a21656ed1d1269847a72897a712e0919
SilviaPlabon <silviaplabon@gmail.com>
2021-08-18 00:34:23 +0600

    add doctor edit

--- src/components/Home/NavBar/NavBar.js
+++ src/components/Home/NavBar/NavBar.js
@@ -102,7 +102,7 @@ const NavBar = () => {
                   </li>
                   <li className="nav-item">
                     <Link className="nav-link" to="/userdashboard">
-                      Find Doctors
+                      Find Doctor
                     </Link>
                   </li>
                   <li className="nav-item">
