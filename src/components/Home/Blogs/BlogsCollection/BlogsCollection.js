8bf303731e515297cb806d901445d5c3b9659694
tahmidmahdi <tahmid5550@gmail.com>
2021-08-19 16:29:09 +0600

    blog section added

--- src/components/Home/Blogs/BlogsCollection/BlogsCollection.js
+++ src/components/Home/Blogs/BlogsCollection/BlogsCollection.js
@@ -1,42 +1,44 @@
-import React from 'react';
-import BlogShow from '../BlogShow/BlogShow'
-import './BlogsCollection.scss'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import BlogShow from '../BlogShow/BlogShow';
+import './BlogsCollection.scss';
 const BlogsCollection = () => {
-    const blogs = [
-        {
-            id:1,
-            date: ' August 01, 2021',
-            name: 'Vaccination',
-            description: 'Hospital management System or HMS Software is mainly a system that designed for multispeciality hospitals, to cover a wide range ofhospital administration and management processes. It is an integrated end-to-end HospitalManagement System that provides relevant information across the hospital to support effectivedecision making for patient care, hospital administration and critical financial accounting, in aseamless flow.Hospital Management System is a software product suite designed to improve the quality andmanagement of hospital management in the areas of clinical process analysis and activity-basedcosting. Hospital Management System enables you to develop your organization and improve itseffectiveness and quality of work. Managing the key processes efficiently is critical to thesuccess of the hospital helps you manage your processes'
-        },
-        {
-            id:2,
-            date: ' August 14, 2021',
-            name: 'E Health',
-            description: 'Hospital management System or HMS Software is mainly a system that designed for multispeciality hospitals, to cover a wide range ofhospital administration and management processes. It is an integrated end-to-end HospitalManagement System that provides relevant information across the hospital to support effectivedecision making for patient care, hospital administration and critical financial accounting, in aseamless flow.Hospital Management System is a software product suite designed to improve the quality andmanagement of hospital management in the areas of clinical process analysis and activity-basedcosting. Hospital Management System enables you to develop your organization and improve itseffectiveness and quality of work. Managing the key processes efficiently is critical to thesuccess of the hospital helps you manage your processes'
-        },
-        {
-            id:3,
-            date: ' August 15, 2021',
-            name: 'Vaccination',
-            description: 'Hospital management System or HMS Software is mainly a system that designed for multispeciality hospitals, to cover a wide range ofhospital administration and management processes. It is an integrated end-to-end HospitalManagement System that provides relevant information across the hospital to support effectivedecision making for patient care, hospital administration and critical financial accounting, in aseamless flow.Hospital Management System is a software product suite designed to improve the quality andmanagement of hospital management in the areas of clinical process analysis and activity-basedcosting. Hospital Management System enables you to develop your organization and improve itseffectiveness and quality of work. Managing the key processes efficiently is critical to thesuccess of the hospital helps you manage your processes'
-        },
-    ]
-    return (
-        <div className="container mb-5 mt-5">
-            <div className="d-flex justify-content-center align-items-center">
-                <hr className="hrStyleBlog me-1"></hr>
-                <h5 className="text-center " style={{ color: '#1dc0bf' }}>Our Blog</h5>
-            </div>
-            <h2 className="text-center mb-2" style={{ color: '#050546' }}>GET OUR DAILY NEWS FEEDS</h2>
-            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-                {
-                    blogs.map(blog => <BlogShow key={blog.id} blog={blog}></BlogShow>)
-                }
-            </div>
-
-        </div>
-    );
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    // http://localhost:4300/blog/getBlog
+    axios
+      .get('https://whispering-reef-28119.herokuapp.com/blog/getBlog')
+      .then(function (response) {
+        // handle success
+        console.log(response.data.result);
+        setBlogs(response.data.result);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }, []);
+  return (
+    <div className="container mb-5 mt-5">
+      <div className="d-flex justify-content-center align-items-center">
+        <hr className="hrStyleBlog me-1"></hr>
+        <h5 className="text-center " style={{color: '#1dc0bf'}}>
+          Our Blog
+        </h5>
+      </div>
+      <h2 className="text-center mb-2" style={{color: '#050546'}}>
+        GET OUR DAILY NEWS FEEDS
+      </h2>
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+        {blogs.map((blog) => (
+          <BlogShow key={blog.id} blog={blog}></BlogShow>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BlogsCollection;
